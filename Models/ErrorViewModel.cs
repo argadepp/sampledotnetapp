40f7cd23bd5ae1a0c62c6cab389af8cc3d7400d5
Pratik Argade <10265@hspl.com>
2022-05-04 11:23:40 +0530

    Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace newdevops.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
